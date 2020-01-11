4eff308da0623ef41ef23c4e5aac3317ea08a5c6
Rashmi Gupta <rashmi224141@gmail.com>
2020-01-11 14:14:50 +0530

    Added basket class, draw basket function

--- collectFruit.js
+++ collectFruit.js
@@ -21,9 +21,9 @@ const generateId = function({colNo, rowNo}) {
   return `${colNo}_${rowNo}`;
 };
 
-function getBox(fruit) {
-  const positions = fruit.getPosition;
-  const id = generateId(positions);
+function getBox(content) {
+  const position = content.getPosition;
+  const id = generateId(position);
   const box = document.getElementById(id);
   return box;
 }
@@ -46,6 +46,15 @@ const drawFruits = function(fruits) {
   });
 };
 
+const drawBasket = function(basket) {
+  const box = getBox(basket);
+  // box.classList.add('basket');
+  const img = document.createElement('img');
+  img.classList.add('basket');
+  img.src = basket.getImage;
+  box.appendChild(img);
+};
+
 const getRandomPosition = function() {
   const colNo = Math.floor(Math.random() * 12) % 12;
   return {colNo: colNo, rowNo: 0};
@@ -55,6 +64,29 @@ const getRandomFruit = function() {
   const fruits = ['apple.png', 'mango.png', 'banana.png', 'img1.png'];
   return fruits[Math.floor(Math.random() * 4)];
 };
+
+class Basket {
+  constructor(position, basket) {
+    this.colNo = position.colNo;
+    this.rowNo = position.rowNo;
+    this.basket = basket;
+  }
+  get getImage() {
+    return this.basket;
+  }
+
+  get getPosition() {
+    return {colNo: this.colNo, rowNo: this.rowNo};
+  }
+
+  moveLeft() {
+    this.colNo--;
+  }
+
+  moveRight() {
+    this.colNo++;
+  }
+}
 
 class Fruit {
   constructor(position, fruit) {
@@ -80,10 +112,12 @@ const main = function() {
   populateCells(grid);
   const fruits = [];
   fruits.push(new Fruit(getRandomPosition(), getRandomFruit()));
+  const basket = new Basket({colNo: 5, rowNo: 11}, 'basket.png');
+  drawBasket(basket);
   drawFruits(fruits);
   setInterval(() => {
     eraseAndMoveFruits(fruits);
     fruits.push(new Fruit(getRandomPosition(), getRandomFruit()));
     drawFruits(fruits);
-  }, 500);
+  }, 800);
 };
