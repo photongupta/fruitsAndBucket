766277ea52b1dc7697a576663ee60ed8924575f0
Rashmi Gupta <rashmi224141@gmail.com>
2020-01-11 09:51:42 +0530

    functions names modified

--- collectFruit.js
+++ collectFruit.js
@@ -1,15 +1,7 @@
-const rowNo = 15;
-const colNo = 12;
-
-const createId = function(x, y) {
-  return `${x}_${y}`;
-};
-
 const createCell = function(x, y) {
   const cell = document.createElement('div');
   cell.className = 'cell';
-  cell.id = createId(x, y);
-  return cell;
+  cell.id = generateId(x, y);
 };
 
 const populateCells = function(grid) {
@@ -21,52 +13,73 @@ const populateCells = function(grid) {
   }
 };
 
-const createRandomId = function() {
-  const colId = Math.floor(Math.random() * 12) % 12;
-  return createId(colId, 0);
+const rowNo = 15;
+const colNo = 12;
+
+const generateId = function(x, y) {
+  return `${x}_${y}`;
 };
+
+const getRandomId = function() {
+  const colId = Math.floor(Math.random() * 12) % 12;
+  return generateId(colId, 0);
+};
+
 const getFruit = function() {
   const fruits = ['apple.png', 'mango.png', 'banana.png', 'img1.png'];
   return fruits[Math.floor(Math.random() * 4)];
-};
-
-const setFruit = function(id, fruit) {
-  const box = document.getElementById(id);
-  const img = document.createElement('img');
-  img.src = fruit;
-  box.appendChild(img);
-  const cell = {id: id, image: img};
-  fruitDetail.push(cell);
 };
 
 const getNextId = function(id) {
   const dim = id.split('_');
   const colNo = +dim[0];
   const rowNo = +dim[1] + 1;
-  return `${colNo}_${rowNo}`;
+  return generateId(colNo, rowNo);
 };
 
-const updateFruitPosition = function(cell) {
-  const box = document.getElementById(cell.id);
-  box.innerHTML = '';
-  const nextId = getNextId(cell.id);
-  const nextBox = document.getElementById(nextId);
-  nextBox.appendChild(cell.image);
-  return {id: nextId, image: cell.image};
+const getFruitDetail = function() {
+  const id = getRandomId();
+  const fruit = getFruit();
+  return {id: id, image: fruit};
 };
 
-let fruitDetail = [];
+const drawFruit = function(fruits) {
+  fruits.forEach(fruit => {
+    const box = document.getElementById(fruit.id);
+    const img = document.createElement('img');
+    img.src = fruit.image;
+    box.appendChild(img);
+  });
+};
 
-const moveFruit = function() {
-  fruitDetail = fruitDetail.map(updateFruitPosition);
+const eraseFruits = function(fruits) {
+  fruits.forEach(fruit => {
+    const box = document.getElementById(fruit.id);
+    const img = box.children[0];
+    box.removeChild(img);
+  });
+};
+
+const increasePosition = function(fruit) {
+  const nextId = getNextId(fruit.id);
+  return {id: nextId, image: fruit.image};
+};
+
+const moveFruit = function(Fruits) {
+  return Fruits.map(increasePosition);
 };
 
 const main = function() {
   const grid = document.getElementById('grid');
   populateCells(grid);
-  setFruit('0_0', 'apple.png');
+  let prevFruitDetail = [];
+  let newFruitDetail = [];
   setInterval(() => {
-    moveFruit();
-    setFruit(createRandomId(), getFruit());
+    eraseFruits(prevFruitDetail);
+    const newFruit = getFruitDetail();
+    newFruitDetail.push(newFruit);
+    drawFruit(newFruitDetail);
+    prevFruitDetail = newFruitDetail.slice();
+    newFruitDetail = moveFruit(newFruitDetail);
   }, 1000);
 };
